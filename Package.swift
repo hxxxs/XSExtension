b1fde6a9bc8d381ce293c2caeee8d1cf76dfc839
hxs <lychae@foxmail.com>
2020-04-20 09:39:53 +0800

    package.swift 文件修改

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "XSExtension",
-            path: "Source"),
+            dependencies: []),
         .testTarget(
             name: "XSExtensionTests",
             dependencies: ["XSExtension"]),
