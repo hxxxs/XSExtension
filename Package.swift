7b9965975adf4249910bdb9ba9a0eae9fb46391e
hxs <lychae@foxmail.com>
2020-04-20 09:23:56 +0800

    修改swift package manager数据源路径

--- Package.swift
+++ Package.swift
@@ -20,9 +20,10 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "XSExtension",
-            dependencies: []),
+            path: "Source"),
         .testTarget(
             name: "XSExtensionTests",
             dependencies: ["XSExtension"]),
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
